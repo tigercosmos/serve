db9b382f8c9cb2a5739147291a592f12cc4d5f0b
tigercosmos <phy.tiger@gmail.com>
2021-01-11 03:45:49 +0800

    [inference test]  send request to serve

--- inference-test/index.js
+++ inference-test/index.js
@@ -1,8 +1,24 @@
-const axious = require("axios")
+const axios = require("axios")
 const logger = require("node-color-log")
+const fs = require("fs");
 
-function main() {
+async function send(deadline, layers, counter) {
+    const res = await axios.put(
+        `htp://127.0.0.1:8080/predictions/googlenet/${deadline}/${layers}`,
+         fs.readFileSync(__dirname + '/kitten_small.jpg'),{
+            headers: {
+              'Content-Type': "jpg"
+            }
+          });
+    logger.info(`Request ${counter}:`, res.data)
+}
 
+async function main() {
+    for(let i = 0; i < 10; i++) {
+        const deadline = 10000000;
+        const layers = "0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1";
+        send(deadline, layers, i);
+    }
 }
 
 main();
